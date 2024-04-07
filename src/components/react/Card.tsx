866e622ad11a0e56451ea5e3111bb6e3c5dccb27
Jesus Guerra <jesus.guerra@alegra.com>
2024-04-07 12:10:46 -0400

    chore: fixing astro build errors

--- src/components/react/Card.tsx
+++ src/components/react/Card.tsx
@@ -1,12 +1,6 @@
 import "./styles/Card.css";
 import "atropos/atropos.css";
 import Atropos from "atropos/react";
-import bg from "./assets/images/atropos-bg.svg";
-import forestBack from "./assets/images/atropos-forest-back.svg";
-import forestFront from "./assets/images/atropos-forest-front.svg";
-import forestMid from "./assets/images/atropos-forest-mid.svg";
-import logo from "./assets/images/atropos-logo-en.svg";
-import mountains from "./assets/images/atropos-mountains.svg";
 
 export default function Card() {
   return (
@@ -19,13 +13,13 @@ export default function Card() {
         onLeave={() => console.log("Leave")}
         onRotate={(x, y) => console.log("Rotate", x, y)}
       >
-        <img className="atropos-banner-spacer" src={bg} alt="" />
-        <img className="" src={bg} alt="" data-atropos-offset="-4" />
-        <img src={mountains} alt="" data-atropos-offset="-4" />
-        <img src={forestBack} alt="" data-atropos-offset="-2" />
-        <img src={forestMid} alt="" data-atropos-offset="0" />
-        <img src={forestFront} alt="" data-atropos-offset="4" />
-        <img src={logo} alt="" data-atropos-offset="5" />
+        <img className="atropos-banner-spacer" src="/assets/images/atropos-bg.svg" alt="" />
+        <img className="" src="/assets/images/atropos-bg.svg" alt="" data-atropos-offset="-4" />
+        <img src="/assets/images/atropos-mountains.svg" alt="" data-atropos-offset="-4" />
+        <img src="/assets/images/atropos-forest-back.svg" alt="" data-atropos-offset="-2" />
+        <img src="/assets/images/atropos-forest-mid.svg" alt="" data-atropos-offset="0" />
+        <img src="/assets/images/atropos-forest-front.svg" alt="" data-atropos-offset="4" />
+        <img src="/assets/images/atropos-logo.svg" alt="" data-atropos-offset="5" />
         <div className="atropos-cta" data-atropos-offset="6">
           <button
             onClick={(е) => {
